b76244148361b66361f7f0239db4fbf1150a4d6b
MSI\USER <kk850408@gmail.com>
2021-09-03 20:16:12 +0800

    煙火

---
+++ iPad/iPhone11.js
@@ -0,0 +1,22 @@
+[
+  { "id":"A001", "color": "白色",  "price": 19900, "storage":"64GB", "picture":"white11.png" },
+  { "id":"A002", "color": "黑色",  "price": 19900, "storage":"64GB", "picture":"black11.png" },
+  { "id":"A003", "color": "綠色",  "price": 19900, "storage":"64GB", "picture":"green11.png" },
+  { "id":"A004", "color": "黃色",  "price": 19900, "storage":"64GB", "picture":"yellow11.png" },
+  { "id":"A005", "color": "紫色",  "price": 19900, "storage":"64GB", "picture":"purple11.png" },
+  { "id":"A006", "color": "紅色",  "price": 19900, "storage":"64GB", "picture":"red11.png" },
+
+  { "id":"B001", "color": "白色",  "price": 21500, "storage":"128GB", "picture":"white11.png" },
+  { "id":"B002", "color": "黑色",  "price": 21500, "storage":"128GB", "picture":"black11.png" },
+  { "id":"B003", "color": "綠色",  "price": 21500, "storage":"128GB", "picture":"green11.png" },
+  { "id":"B004", "color": "黃色",  "price": 21500, "storage":"128GB", "picture":"yellow11.png" },
+  { "id":"B005", "color": "紫色",  "price": 21500, "storage":"128GB", "picture":"purple11.png" },
+  { "id":"B006", "color": "紅色",  "price": 21500, "storage":"128GB","picture":"red11.png" },
+
+  { "id":"C001", "color": "白色",  "price": 25000, "storage":"256GB", "picture":"white11.png" },
+  { "id":"C002", "color": "黑色",  "price": 25000, "storage":"256GB", "picture":"black11.png" },
+  { "id":"C003", "color": "綠色",  "price": 25000, "storage":"256GB", "picture":"green11.png" },
+  { "id":"C004", "color": "黃色",  "price": 25000, "storage":"256GB", "picture":"yellow11.png" },
+  { "id":"C005", "color": "紫色",  "price": 25000, "storage":"256GB", "picture":"purple11.png" },
+  { "id":"C006", "color": "紅色",  "price": 25000, "storage":"256GB", "picture":"red11.png" },
+]
